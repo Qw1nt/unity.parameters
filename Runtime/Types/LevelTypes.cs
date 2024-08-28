3ce34bab3c795dcdb641396b478d433664a83f3e
Qw1nt <pasharudnykh3@mail.ru>
2024-08-28 18:32:14 +0500

    [*]

---
+++ Runtime/Types/LevelTypes.cs
@@ -0,0 +1,11 @@
+using Parameters.Runtime.Attributes;
+using Parameters.Runtime.Common;
+
+namespace Plugins.Parameters.Runtime.Types
+{
+    [ParameterCrateDescription(typeof(SingleIntDescriptor))]
+    public partial class MapSize
+    {
+    
+    }
+}
