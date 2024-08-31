aec9a2846f5eee3440e5986e4292ce4fdb837dd7
Qw1nt <pasharudnykh3@mail.ru>
2024-08-31 15:06:02 +0500

    [+] Стандартный DockerHolder

---
+++ Runtime/Common/MonoDockerHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Parameters.Runtime.Interfaces;
+using UnityEngine;
+
+namespace Parameters.Runtime.Common
+{
+    [DefaultExecutionOrder(-100)]
+    public class MonoDockerHolder : MonoBehaviour, IDockerHolder
+    {
+        [SerializeField] private ParametersDockerData.ParameterInDocker[] _parameters;
+        
+        public ParameterDocker Docker
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+            private set;
+        }
+        
+        private void Awake()
+        {
+            Docker = new ParameterDocker(this, _parameters);
+        }
+    }
+}
